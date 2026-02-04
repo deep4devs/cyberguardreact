a63a6f290053e8dc770521fcb1716264b3f8e3af
deep4devs <webmaster@elcompa.digital>
2026-02-04 16:12:51 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import './polyfills'; // Add this line at the very top
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './polyfills'; // ADD THIS LINE AT THE VERY TOP
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
