8b10d2432207c99d3e3390151f87ad5b12316393
deep4devs <webmaster@elcompa.digital>
2026-02-04 17:09:08 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import './polyfills'; // First line
+import './polyfills'; // MUST BE FIRST LINE - critical for buffer
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // THIS IMPORT MUST EXIST
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
