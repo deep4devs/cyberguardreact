45b611746d6f849d7e649f4cd6e4c594e60520a0
deep4devs <webmaster@elcompa.digital>
2026-02-04 17:06:25 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +12,30 @@ export default defineConfig({
     outDir: 'dist',
   },
   optimizeDeps: {
-    include: ['react', 'react-dom'],
+    include: ['react', 'react-dom', 'buffer'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        {
+          name: 'buffer',
+          setup(build) {
+            build.onResolve({ filter: /^buffer$/ }, () => ({
+              path: require.resolve('buffer/'),
+            }))
+          },
+        },
+      ],
+    }
   },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': resolve(__dirname, './src'),
+      'buffer': 'buffer'
     },
-  },
+    fallback: {
+      buffer: require.resolve('buffer/')
+    }
+  }
 })
